8d479722783ec29577b2ffb5fd61019b71568b67
Meeta Haldar <69325431+MeetaHaldar@users.noreply.github.com>
2021-05-20 20:06:01 +0530

    Add files via upload

---
+++ 01_dataType_and_range.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x;
+    char a;
+    cout << "size of int = " << sizeof(x) << " bytes" << endl;
+    cout << INT8_MIN << "  to  " << INT8_MAX;
+
+    return 0;
+}
